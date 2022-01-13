f3304bb97d6ae7bcfd19c30bf3b197c351495981
Benjamin Goddard <bdgcypher@gmail.com>
2022-01-12 22:05:18 -0700

    Fixed something in the navbar and made the dataset_selection section of dashboard

--- components/dashboard.tsx/dataset_selection.tsx
+++ components/dashboard.tsx/dataset_selection.tsx
@@ -1,75 +1,64 @@
 import React from 'react';
-import { CalendarIcon, LocationMarkerIcon, UsersIcon } from '@heroicons/react/solid'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsImage } from 'react-icons/bs'
+import { AiOutlineCloudUpload } from 'react-icons/ai'
 
-const positions = [
-  {
-    id: 1,
-    title: 'Back End Developer',
-    type: 'Full-time',
-    location: 'Remote',
-    department: 'Engineering',
-    closeDate: '2020-01-07',
-    closeDateFull: 'January 7, 2020',
-  },
-  {
-    id: 2,
-    title: 'Front End Developer',
-    type: 'Full-time',
-    location: 'Remote',
-    department: 'Engineering',
-    closeDate: '2020-01-07',
-    closeDateFull: 'January 7, 2020',
-  },
-  {
-    id: 3,
-    title: 'User Interface Designer',
-    type: 'Full-time',
-    location: 'Remote',
-    department: 'Design',
-    closeDate: '2020-01-14',
-    closeDateFull: 'January 14, 2020',
-  },
+const datasets = [
+    {
+        id: 1,
+        title: 'Personal Emails',
+        type: 'Email',
+    },
+    {
+        id: 2,
+        title: 'Fire Dataset',
+        type: 'Image',
+    },
 ]
 
 export default function DatasetSelection() {
-  return (
-    <div className="bg-white shadow overflow-hidden sm:rounded-md">
-      <ul role="list" className="divide-y divide-gray-200">
-        {positions.map((position) => (
-          <li key={position.id}>
-            <a href="#" className="block hover:bg-gray-50">
-              <div className="px-4 py-4 sm:px-6">
-                <div className="flex items-center justify-between">
-                  <p className="text-sm font-medium text-indigo-600 truncate">{position.title}</p>
-                  <div className="ml-2 flex-shrink-0 flex">
-                    <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                      {position.type}
-                    </p>
-                  </div>
-                </div>
-                <div className="mt-2 sm:flex sm:justify-between">
-                  <div className="sm:flex">
-                    <p className="flex items-center text-sm text-gray-500">
-                      <UsersIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                      {position.department}
-                    </p>
-                    <p className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6">
-                      <LocationMarkerIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                      {position.location}
-                    </p>
-                  </div>
-                  <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                    <CalendarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    <p>
-                      Closing on <time dateTime={position.closeDate}>{position.closeDateFull}</time>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+    return (
+        <div className="bg-white shadow overflow-hidden sm:rounded-md">
+            <ul role="list" className="divide-y divide-gray-200">
+                <li key={0}>
+                    <a href="#" className="block hover:bg-gray-50">
+                        <div className="px-4 py-4 sm:px-6">
+                            <div className="flex items-center justify-between">
+                                <p className="text-lg font-medium text-gray-900 truncate">Your Datasets</p>
+                                <div className="ml-2 flex-shrink-0 flex">
+                                    <button
+                                        type="button"
+                                        className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                                    >
+                                        Upload Dataset
+                                        <AiOutlineCloudUpload className="flex-shrink-0 ml-1.5 h-5 w-5 text-white" aria-hidden="true" />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                {datasets.map((dataset) => (
+                    <li key={dataset.id}>
+                        <a href="#" className="block hover:bg-gray-50">
+                            <div className="px-4 py-4 sm:px-6">
+                                <div className="flex items-center justify-between">
+                                    <p className="text-sm font-medium text-blue-600 truncate">{dataset.title}</p>
+                                </div>
+                                <div className="mt-2 sm:flex sm:justify-between">
+                                    <div className="sm:flex">
+                                        <p className="flex items-center text-sm text-gray-500">
+                                            {dataset.type === 'Email' && <HiOutlineMail className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />}
+                                            {dataset.type === 'Image' && <BsImage className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />}
+                                            {dataset.type}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -77,19 +77,6 @@ export default function Navbar() {
                                 'block px-4 py-2 text-sm text-gray-700'
                               )}
                             >
-                              Settings
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700'
-                              )}
-                            >
                               Sign out
                             </a>
                           )}
@@ -129,13 +116,6 @@ export default function Navbar() {
                 </button>
               </div>
               <div className="mt-3 px-2 space-y-1">
-                <Disclosure.Button
-                  as="a"
-                  href="#"
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-                >
-                  Settings
-                </Disclosure.Button>
                 <Disclosure.Button
                   as="a"
                   href="#"
