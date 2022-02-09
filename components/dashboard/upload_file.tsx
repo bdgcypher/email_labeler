0cce49c36155910dee16e369a7479971cb282327
Benjamin Goddard <bdgcypher@gmail.com>
2022-02-09 10:10:54 -0700

    potential fixes to CORS on upload

--- components/dashboard/upload_file.tsx
+++ components/dashboard/upload_file.tsx
@@ -136,6 +136,33 @@ class FileUploader extends Component {
           ) : (
             resolve(408)
           )
+        }).catch(function (error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+            {
+              error.response.status === 401 ? (
+                  window.location.replace('/login')
+              ) : error.response.status === 408 ? (
+                this.setState({ uploadStarted: false, uploadError: true }),
+                console.log(error.response.message)
+              ) : (console.log(error.response.message))
+            }
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+            this.setState({ uploadStarted: false, uploadError: true })
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log(error.message);
+            this.setState({ uploadStarted: false, uploadError: true })
+          }
+          console.log(error.config);
         })
     });
   }
